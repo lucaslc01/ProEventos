02b2b122ca25bb0b7658a86f569de446f36cd280
Lucas Muniz <lucas@CMU.LOCAL>
2022-04-27 16:25:06 -0300

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+            EventoId = 1,
+            Tema = "Angular 13 e .NET 5",
+            Local = "BH",
+            Lote = "1",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+            ImagemURL = "foto.png"
+            },
+            
+            new Evento(){
+            EventoId = 2,
+            Tema = "Angular e suas novidades",
+            Local = "SP",
+            Lote = "2",
+            QtdPessoas = 350,
+            DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+            ImagemURL = "foto1.png"
+            }
+        };
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+    }
+}
